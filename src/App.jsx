1d8aafaf7447293e514d1833a974ad134adaf65a
gideonadjei <gideonadjei94@gmail.com>
2025-07-22 15:37:12 +0000

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <p className="text-xl font-semibold ">
+        Hello 👋, Welcome to <span className="text-indigo-600">TalentLoop</span>
+      </p>
+    </div>
+  );
+}
+
+export default App;
